3c8f118d9a2bd94fe197e4d6cbc0dfb7cb594101
fernandamackias <115710123+fernandamackias@users.noreply.github.com>
2023-04-03 21:51:04 -0300

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,13 @@
+function relogio() {
+    const elementoRelogio = document.querySelector(".relogio1");
+    const horaAtual = new Date();// informações referente ao tempo
+    const horas = horaAtual.getHours(); //pegas as horas
+    const minutos = horaAtual.getMinutes();//pegas os minutos
+    const segundos = horaAtual.getSeconds();//pegar os segundos 
+    const formatoHoras = horas.toString ().padStart(2,"0"); //formato das horas
+    const formatoMinutos = minutos.toString().padStart(2,"0");
+    const formatoSegundos = segundos.toString().padStart(2,"0");
+    elementoRelogio.textContent = `${formatoHoras}:${formatoMinutos}:${formatoSegundos}`
+
+}
+setInterval(relogio, 1000)
